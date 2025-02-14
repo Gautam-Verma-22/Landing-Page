d24d53a9d7ae14df2dd8a7f91ff015ad5f3d97a6
Garv <vermag667@gmail.com>
2025-02-14 18:06:02 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import Section1 from './Components/Section1'
+import Section2 from './Components/Section2'
+import Section3 from './Components/Section3'
+import Section4 from './Components/Section4'
+import Footer from './Components/Footer'
+
+const page = () => {
+  return (
+    <>
+      <Section1 />
+      <Section2 />  
+      <Section3/>
+      <Section4/>
+      <Footer/>
+    </>
+  )
+}
+
+export default page
